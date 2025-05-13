dd6c2386210dd64f918fc7c883a23c9a5b0ad81e
Ma Theresa Credo <ma.delmundo@globe.com.ph>
2025-05-13 15:03:16 +0800

    add instance scheduler

--- rds-aurora-postgresql.tf
+++ rds-aurora-postgresql.tf
@@ -42,4 +42,52 @@ module "aurora_module" {
       value = "aurpsql-instance"
     }
   ]*/
+
+  use_default_schedule      = false
+  #Stop instances from 12midnight - 5:59AM (Mon-Sat)
+  instance_scheduler_period = [
+    {
+        period_name = "start-6am-mon"
+        description = "This period will start instances at 6 AM and stop at 11:59 PM on Monday"
+        begintime   = "06:00"
+        endtime     = "23:59"
+        weekdays    = ["mon"]
+    },
+    {
+        period_name = "start-6am-tue"
+        description = "This period will start instances at 6 AM and stop at 11:59 PM on Tuesday"
+        begintime   = "06:00"
+        endtime     = "23:59"
+        weekdays    = ["tue"]
+    },
+    {
+        period_name = "start-6am-wed"
+        description = "This period will start instances at 6 AM and stop at 11:59 PM on Wednesday"
+        begintime   = "06:00"
+        endtime     = "23:59"
+        weekdays    = ["wed"]
+    },
+    {
+        period_name = "start-6am-thu"
+        description = "This period will start instances at 6 AM and stop at 11:59 PM on Thursday"
+        begintime   = "06:00"
+        endtime     = "23:59"
+        weekdays    = ["thu"]
+    },
+    {
+        period_name = "start-6am-fri"
+        description = "This period will start instances at 6 AM and stop at 11:59 PM on Thursday"
+        begintime   = "06:00"
+        endtime     = "23:59"
+        weekdays    = ["fri"]
+    },
+    {
+        period_name = "start-6am-sat"
+        description = "This period will start instances at 6 AM and stop at 11:59 PM on Saturday"
+        begintime   = "06:00"
+        endtime     = "23:59"
+        weekdays    = ["sat"]
+    }
+  ]
+
 }
