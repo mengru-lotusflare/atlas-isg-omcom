d4facab596d0644f90a41fdb0e90717df46606fc
Mark Jayson Cruz <mark.cruz@globe.com.ph>
2025-05-26 15:44:16 +0800

    aurora postgre and mysql

--- rds-aurora-postgresql.tf
+++ rds-aurora-postgresql.tf
@@ -13,38 +13,39 @@ module "aurora_module_petal" {
   engine_version     = "16.4"
   parameter_group_name = "aurora-postgresql16"
   # database_name = "dnopetal"
-  /*allow_ingress_from = []
+  # allow_ingress_from = []
 
-  enable_alarms = [
-    "CPUUtilization", "FreeableMemory", "DatabaseConnections", "RDSToAuroraPostgreSQLReplicaLag", "ReadIOPS",
-    "WriteIOPS", "SnapshotStorageUsed", "SwapUsage"
-  ]
-  aurora_cpu_threshold                   = 70 #percent
-  aurora_memory_threshold                = 3758096384 #in bytes, 3.5gb
-  aurora_databaseconnection_threshold    = 10 #number of database connection
-  aurora_replica_lag_threshold           = 120 #seconds
-  aurora_read_iops_threshold             = 10 #readiops count per sec
-  aurora_write_iops_threshold            = 10 #write iops count per sec
-  aurora_snapshot_storage_used_threshold = 3758096384 #in bytes, 3.5gb
-  cloudwatch_metric_period               = 60 #seconds
-  allow_iam_read                         = ["prn::srcd2-showcase-sit-rt-role", "srcd2-showcase-sit-ingress-role"]
-  number_of_read_replicas                = 1 # Max 3 allowed
+  # enable_alarms = [
+  #   "CPUUtilization", "FreeableMemory", "DatabaseConnections", "RDSToAuroraPostgreSQLReplicaLag", "ReadIOPS",
+  #   "WriteIOPS", "SnapshotStorageUsed", "SwapUsage"
+  # ]
+  # aurora_cpu_threshold                   = 70 #percent
+  # aurora_memory_threshold                = 3758096384 #in bytes, 3.5gb
+  # aurora_databaseconnection_threshold    = 10 #number of database connection
+  # aurora_replica_lag_threshold           = 120 #seconds
+  # aurora_read_iops_threshold             = 10 #readiops count per sec
+  # aurora_write_iops_threshold            = 10 #write iops count per sec
+  # aurora_snapshot_storage_used_threshold = 3758096384 #in bytes, 3.5gb
+  # cloudwatch_metric_period               = 60 #seconds
+  # allow_iam_read                         = ["prn::srcd2-showcase-sit-rt-role", "srcd2-showcase-sit-ingress-role"]
+  # number_of_read_replicas                = 1 # Max 3 allowed
   cluster_parameters                     = [
     {
-      name  = "application_name"
-      value = "aurpsql-hscl"
-    },
-    {
-      name  = "rds.log_retention_period"
-      value = "5400"
+      name  = "track_counts"
+      value = 0
+
     }
+    # {
+    #   name  = "rds.log_retention_period"
+    #   value = "5400"
+    # }
   ]
-  instance_parameters = [
-    {
-      name  = "application_name"
-      value = "aurpsql-instance"
-    }
-  ]*/
+  # instance_parameters = [
+  #   {
+  #     name  = "application_name"
+  #     value = "aurpsql-instance"
+  #   }
+  # ]
   
    #instance_parameters = [
     # {
