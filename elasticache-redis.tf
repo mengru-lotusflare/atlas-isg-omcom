8860f9db36d07a65de141714d41a684f84812e2d
Mark Jayson Cruz <mark.cruz@globe.com.ph>
2025-05-30 09:38:31 +0800

    COMB-166: Provision Valkey

--- elasticache-redis.tf
+++ elasticache-redis.tf
@@ -1,27 +1,27 @@
-module "elasticache_module_cluster_redis" {
-  source = "globe.pe.jfrog.io/hmd-terraform-local__service/aws-elasticache-redis/aws"
+# module "elasticache_module_cluster_redis" {
+#   source = "globe.pe.jfrog.io/hmd-terraform-local__service/aws-elasticache-redis/aws"
 
-  providers = {
-    aws.security    = aws.security,
-    aws.environment = aws.environment,
-    aws.route53     = aws.route53
-  }
+#   providers = {
+#     aws.security    = aws.security,
+#     aws.environment = aws.environment,
+#     aws.route53     = aws.route53
+#   }
 
-  name      = "redis"
-  node_type = "cache.m5.xlarge"
-  /*cluster_mode_enabled  = true
-  parameter_group_name  = "default.redis7"
-  family                = "redis7"
-  allow_ingress_from    = ["sa::app"]
-  number_cache_clusters = 1 
-  subnet_type           = "elasticcache" # Consult AWS tribe team before modifying this value. 
+#   name      = "redis"
+#   node_type = "cache.m5.xlarge"
+#   /*cluster_mode_enabled  = true
+#   parameter_group_name  = "default.redis7"
+#   family                = "redis7"
+#   allow_ingress_from    = ["sa::app"]
+#   number_cache_clusters = 1 
+#   subnet_type           = "elasticcache" # Consult AWS tribe team before modifying this value. 
 
-  maintenance_window = "sun:05:00-sun:09:00"  # Format: day:start-end
+#   maintenance_window = "sun:05:00-sun:09:00"  # Format: day:start-end
   
-  parameters = [
-   {
-      name  = "notify-keyspace-events"
-      value = "lK"
-    }
- ]*/
-}
+#   parameters = [
+#    {
+#       name  = "notify-keyspace-events"
+#       value = "lK"
+#     }
+#  ]*/
+# }
