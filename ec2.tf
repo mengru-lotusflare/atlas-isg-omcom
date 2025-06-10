e5f3f02e79b38cb7801260be5ab2f23f94072700
Ma Theresa Credo <ma.delmundo@globe.com.ph>
2025-06-10 10:16:46 +0800

    COMB-184: EC2 provisioning

--- ec2.tf
+++ ec2.tf
@@ -8,14 +8,7 @@ module "ec2_module" {
   }
   name = "ec2"
   is_public = false
-  instance_type  = var.instance_type5
-  subnet_type = var.ec2_subnet_type
-  ami = var.ami5
-  custom_ami_owner = var.custom_ami_owner
-  launch_script_path = var.launch_script_path
-  private_ip_address = var.private_ip_address["node5"]
-  ec2_subnet_name = var.ec2_subnet_name["sub2"]
+  ami = "app1"
   deployment_package_zip_path = var.deployment_package_zip_path
-  security_configurations = var.security_configurations
   log_file_paths = var.log_file_paths
 }
