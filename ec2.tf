9a9f055d7d61a366dfa02f301ca58b5005794742
Ma Theresa Credo <ma.delmundo@globe.com.ph>
2025-06-10 10:09:00 +0800

    COMB-184: EC2 provisioning

--- ec2.tf
+++ ec2.tf
@@ -6,39 +6,16 @@ module "ec2_module" {
     aws.security    = aws.security
     aws.route53     = aws.route53
   }
-
   name = "ec2"
   is_public = false
-
-  # uses workload AMI
-  ami = {
-    name = "app1" # ami name as defined in projects
-    build = "1"
-    type = "al2"
-  }
-
-  #Path to zip file containing application deployment folder
-  deployment_package_zip_path = "${path.root}/app.zip"
-
-  #List of file paths to pipe logs to CloudWatch
-  log_file_paths = [
-        "/var/omcom/app.log",
-        "/var/omcom/error.log"
-  ]
-
-  # Specify if you want to use spot instance in launch template. If dont want to use spot instance skip this block.  
-  
- #  max_price - The maximum hourly price you're willing to pay for the Spot Instances.
- 
-  #instance_market_options = [
-  #{
-  # market_type = "spot"
-  #  spot_options = [
-  #    {
-  #      max_price = 0.02
-  #    }
-  #  ]
-  #}
-#]
-
+  instance_type  = var.instance_type5
+  subnet_type = var.ec2_subnet_type
+  ami = var.ami5
+  custom_ami_owner = var.custom_ami_owner
+  launch_script_path = var.launch_script_path
+  private_ip_address = var.private_ip_address["node5"]
+  ec2_subnet_name = var.ec2_subnet_name["sub2"]
+  deployment_package_zip_path = var.deployment_package_zip_path
+  security_configurations = var.security_configurations
+  log_file_paths = var.log_file_paths
 }
